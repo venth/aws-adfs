6807ac5e5ad9b6b647c6b36ab3b29fd5e50c73bc
Patrick Decat <pdecat@gmail.com>
2022-10-10 11:20:14 +0200

    Add support for Duo Verified Push MFA

--- aws_adfs/_duo_universal_prompt_authenticator.py
+++ aws_adfs/_duo_universal_prompt_authenticator.py
@@ -288,6 +288,14 @@ def _verify_authentication_status(duo_host, sid, txid, session, ssl_verification
                 "There was an issue during second factor verification. The error response: {}".format(response.text)
             )
 
+        if json_response["response"]["status_code"] == "pushed":
+            verification_code = json_response["response"].get("risk_based_factor_selection_data", {}).get("step_up_code")
+            if verification_code:
+                click.echo(
+                    f"Duo Verified Push MFA code: {verification_code}",
+                    err=True,
+                )
+
         if json_response["response"]["status_code"] in ["pushed", "answered", "allow"]:
             return txid
 
